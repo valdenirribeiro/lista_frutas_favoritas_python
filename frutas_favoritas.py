bc52c845efb5644c94391b42ddf45e5b4a022946
valdenirribeiro <valdenirdm.ribeiro@protonmail.com>
2024-02-01 11:36:19 -0300

    primeiro commit

---
+++ frutas_favoritas.py
@@ -0,0 +1,12 @@
+frutas_favoritas = ['banana', 'maçã', 'melancia']
+for fruta in frutas_favoritas:
+    if fruta == 'banana':
+        print('Sim banana consta na lista de frutas favoritas.')
+    if fruta == 'pera':
+        print('Sim pera consta na lista de frutas favoritas.')
+    if fruta == 'maçã':
+        print('Sim maçã consta na lista de frutas favoritas.')
+    if fruta == 'manga':
+        print('Não manga não consta na lista de frutas favoritas.')
+    if fruta == 'melancia':
+        print('Sim melancia consta na lista de frutas favoritas.')            
